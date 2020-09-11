3256b4c61388d8fe4e540f7244bd2e57ae49cb5d
Dop-dd <dopdd@yahoo.com>
2020-09-11 09:48:27 +0000

    first commit

---
+++ routes.py
@@ -0,0 +1,55 @@
+import os
+from flask import Flask, render_template, request, flash
+from forms import ContactForm
+from flask_mail import Mail, Message
+
+mail = Mail()
+
+app = Flask(__name__)
+
+app.secret_key = 'development key'
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'milestone2020.projects@gmail.com'
+app.config["MAIL_PASSWORD"] = 'Milestone-2020'
+
+mail.init_app(app)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact',  methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    if request.method == "POST":
+        if form.validate() is False:
+            flash('All fields are required.')
+            return render_template('contact.html', form=form)
+        else:
+            msg = Message(form.subject.data, sender='milestone2020.projects@gmail.com', recipients=['milestone2020.projects@gmail.com'])
+            msg.body = """
+            From: %s <%s>
+             %s
+            """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+
+            return 'Form posted.'
+
+    elif request.method == "GET":
+        return render_template('contact.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=os.environ.get('PORT'), debug=True)
